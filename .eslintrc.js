91b9c215979fba216c7e1f555fa0dc5ae9683723
Nedjim <nedjim.danimon@gmail.com>
2017-09-02 08:55:38 +0200

    add react test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,12 @@ module.exports = {
     "extends": "airbnb",
     "rules" : {
         "semi": [2, "never"],
-        "no-unexpected-multiline": 2
-    }
+        "no-unexpected-multiline": 2,
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
+    },
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
 }
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => (
+  <div>
+    <h1>Hello to you! </h1>
+  </div>
+)
+
+export default Home
